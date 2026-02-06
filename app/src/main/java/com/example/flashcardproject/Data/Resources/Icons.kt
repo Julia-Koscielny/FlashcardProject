7c205979f92a6338d545ad2652382e11c2fd4d41
Julia-Koscielny <pazur6425@gmail.com>
2026-02-06 09:15:30 +0100

    Flashcard creation

---
+++ app/src/main/java/com/example/flashcardproject/Data/Resources/Icons.kt
@@ -0,0 +1,12 @@
+package com.example.flashcardproject.Data.Resources
+
+import androidx.annotation.DrawableRes
+import com.example.flashcardproject.R
+
+enum class Icons(@DrawableRes val res:Int) {
+    FOLDER(R.drawable.folder),
+    FLOWER(R.drawable.flower),
+    STORE(R.drawable.store),
+    SEARCH(R.drawable.search),
+    FAVOURITES(R.drawable.favourites)
+}
